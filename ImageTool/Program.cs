1d078dd39f96ad64fc2a4351f644456f80c85169
Peter Thoman <peter@ph3.at>
2022-02-20 20:03:41 +0100

    VERSIONgit diff!

--- ImageTool/Program.cs
+++ ImageTool/Program.cs
@@ -2,7 +2,7 @@ namespace ImageTool
 {
     internal static class Program
     {
-        public readonly static string VERSION = "0.1";
+        public readonly static string VERSION = "0.5";
 
         /// <summary>
         ///  The main entry point for the application.
